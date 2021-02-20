112226c5a7e648369097a3b8fb55086bc35912c0
Souravsinha96 <souravsinha1604@gmail.com>
2021-02-20 20:53:23 +0530

    Day18 Background slider

---
+++ Background-slider/script.js
@@ -0,0 +1,30 @@
+const body = document.body;
+const slides = document.querySelectorAll(".slide");
+const rightBtn = document.getElementById("right");
+const leftBtn = document.getElementById("left");
+let activeSlide = 0;
+
+rightBtn.addEventListener("click", () => {
+  activeSlide++;
+  if (activeSlide > slides.length - 1) {
+    activeSlide = 0;
+  }
+  setBgTobody();
+  setActiveSlide();
+});
+leftBtn.addEventListener("click", () => {
+  activeSlide--;
+  if (activeSlide < 0) {
+    activeSlide = slides.length - 1;
+  }
+  setBgTobody();
+  setActiveSlide();
+});
+setBgTobody();
+function setBgTobody() {
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+function setActiveSlide() {
+  slides.forEach((slide) => slide.classList.remove("active"));
+  slides[activeSlide].classList.add("active");
+}
